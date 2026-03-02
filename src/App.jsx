668dc6862604dd50c381459f12adbacf5e0a1d8d
erica <javilloericajane@gmail.com>
2026-03-02 08:43:27 +0800

    ni pull q

--- src/App.jsx
+++ src/App.jsx
@@ -34,21 +34,21 @@ function App() {
       </nav>
       <section id='home' className='min-h-screen flex items-center px-6 bg-cover bg-no-repeat' style={{ backgroundImage: `url(${bg})`}}>
         <div className='max-w-7xl mx-auto w-full flex flex-col-reverse md:flex-row items-center justify justify-between gap-12'>
-          <div className='text-center md:text-left max-w-xl'>
-        <h2 className='text-5xl md:text-7xl font-bold text-pink-700 leading-tight'>Hi! I'm Jane 🩷 </h2>
-        <p className='mt-6 text-lg text-pink-700 text-center'> A passionate frontend developer who loves creating interactive web experiences</p>
-          </div>
-             <div className='relative'>
-              <img src= {jane} alt='eme' className='w-lg md:w[450px] drop-shadow-2xl animate-[float_4s_ease-in-out_infinite]'/>
-              <div className='absolute inset-0 bg-pink-300/20 blur-3xl rounded-full'></div>
-             </div>
-             <div className="flex flex-col self-end gap-6 mt-8">
-                <a href="https://github.com/ericaa-aaa" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaGithub/></a>
-                <a href="https://www.facebook.com/jane.javillo226" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaFacebook/></a>
-                <a href="https://www.instagram.com/janejavs_/" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaInstagram/></a>
-                <a href="https://x.com/janejavs_" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaTwitter/></a>
-                <a href="https://ph.pinterest.com/janejavillo_/" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaPinterest/></a>
-             </div>   
+            <div className='text-center md:text-left max-w-xl'>
+                  <h2 className='text-5xl md:text-7xl font-bold text-pink-700 leading-tight'>Hi! I'm Jane 🩷 </h2>
+                  <p className='mt-6 text-lg text-pink-700 text-center'> A passionate frontend developer who loves creating interactive web experiences</p>
+            </div>
+            <div className='relative'>
+                  <img src= {jane} alt='eme' className='w-lg md:w[450px] drop-shadow-2xl animate-[float_4s_ease-in-out_infinite]'/>
+                  {/* <div className='absolute inset-0 bg-pink-300/20 blur-3xl rounded-full'></div>  */}
+            </div>
+            <div className="flex flex-col self-end gap-6 mt-8">
+                  <a href="https://github.com/ericaa-aaa" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaGithub/></a>
+                  <a href="https://www.facebook.com/jane.javillo226" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaFacebook/></a>
+                  <a href="https://www.instagram.com/janejavs_/" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaInstagram/></a>
+                  <a href="https://x.com/janejavs_" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaTwitter/></a>
+                  <a href="https://ph.pinterest.com/janejavillo_/" target='_blank' rel='noopener noreferrer' className='text-3xl text-pink-500 hover:text-white hover:bg-pink-500 p-3 rounded-full shadow-lg hover:scale-125 transition duration-300'><FaPinterest/></a>
+            </div>   
         </div>
       </section>
       
@@ -60,9 +60,9 @@ function App() {
         </p>
       </section>
 
-      <section id='projects' className='min-h-screen flex flex-col justify-center items-center py-20 px-6 text-center bg-gradient-to-br from-pink-100 via-rose-200 to-pink-300'>
+      <section id='projects' className='min-h-screen flex flex-col justify-center items-center py-20 px-6 text-center bg-linear-to-br from-pink-100 via-rose-200 to-pink-300'>
         <h3 className='text-3xl font-bold mb-12'>My Project 🎀 </h3>
-        <div className='flex gap-6 overflow-x-auto sanp-x snap-mandatory scroll-smooth pb-4'>
+        <div className='grid grid-cols-3 gap-6 overflow-x-auto snap-x snap-mandatory scroll-smooth pb-4'>
           {[first,second,third,fourth,fifth].map((image, index) => (
             <div key='index' className='min-w-75'>
               <div className='rounded-3xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-100'>
